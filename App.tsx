fb458d74878fa87885f39e5cbf760b56b41650a3
abdelrahmanayaad <abdelrahmanayad74@gmail.com>
2025-10-15 23:40:17 +0300

    change color of status bar

--- App.tsx
+++ App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import MainStack from './src/navigation';
 import Provider from './src/provider';
 import { StatusBar } from 'react-native';
+import { COLORS } from './src/utils/theme';
 
 const App = () => {
   return (
     <Provider>
-      <StatusBar barStyle={'dark-content'} />
+      <StatusBar backgroundColor={COLORS.primary} barStyle={'light-content'} />
       <MainStack />
     </Provider>
   );
